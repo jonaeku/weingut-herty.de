45553090e2c3aa04fe46d19799b881070d70d613
Jona Kuhn <jonaeku@gmx.de>
2021-11-15 11:04:56 +0100

    initial push

---
+++ js.js
@@ -0,0 +1,7 @@
+function toggleMenu() {
+    const toggle = document.querySelector(".nav-button")
+    toggle.classList.toggle("active")
+    const navMenu = document.querySelector(".nav-menu")
+    navMenu.classList.toggle("active")
+}
+
